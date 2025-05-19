43b8ff2152afd41ad7ef80c287c651581d7ee2ba
Amandickz <amandickz@gmail.com>
2025-05-19 17:17:30 -0300

    Criação de tela para teste

--- Projeto-v1/src/main/java/classes/Administracao.java
+++ Projeto-v1/src/main/java/classes/Administracao.java
@@ -14,22 +14,43 @@ public class Administracao {
     private String cnpj;
     private String dataDeAbertura;
     private String razaoSocial;
+    private String nomeFantasia;
+    private int porte;
+    private String email;
+    private String telefone;
+    private int situacaoCadastral;
+    private String dataDaSituacao;
+    private int indicadorMatriz;
 
     public Administracao() {
     }
 
-    public Administracao(int id, String cnpj, String dataDeAbertura, String razaoSocial) {
+    public Administracao(String cnpj, String dataDeAbertura, String razaoSocial, String nomeFantasia, int porte, String email, String telefone, int situacaoCadastral, String dataDaSituacao, int indicadorMatriz) {
+        this.cnpj = cnpj;
+        this.dataDeAbertura = dataDeAbertura;
+        this.razaoSocial = razaoSocial;
+        this.nomeFantasia = nomeFantasia;
+        this.porte = porte;
+        this.email = email;
+        this.telefone = telefone;
+        this.situacaoCadastral = situacaoCadastral;
+        this.dataDaSituacao = dataDaSituacao;
+        this.indicadorMatriz = indicadorMatriz;
+    }
+
+    public Administracao(int id, String cnpj, String dataDeAbertura, String razaoSocial, String nomeFantasia, int porte, String email, String telefone, int situacaoCadastral, String dataDaSituacao, int indicadorMatriz) {
         this.id = id;
         this.cnpj = cnpj;
         this.dataDeAbertura = dataDeAbertura;
         this.razaoSocial = razaoSocial;
-    }
-
-    public Administracao(String cnpj, String dataDeAbertura, String razaoSocial) {
-        this.cnpj = cnpj;
-        this.dataDeAbertura = dataDeAbertura;
-        this.razaoSocial = razaoSocial;
-    }
+        this.nomeFantasia = nomeFantasia;
+        this.porte = porte;
+        this.email = email;
+        this.telefone = telefone;
+        this.situacaoCadastral = situacaoCadastral;
+        this.dataDaSituacao = dataDaSituacao;
+        this.indicadorMatriz = indicadorMatriz;
+    }    
 
     public int getId() {
         return id;
@@ -63,9 +84,65 @@ public class Administracao {
         this.razaoSocial = razaoSocial;
     }
 
+    public String getNomeFantasia() {
+        return nomeFantasia;
+    }
+
+    public void setNomeFantasia(String nomeFantasia) {
+        this.nomeFantasia = nomeFantasia;
+    }
+
+    public int getPorte() {
+        return porte;
+    }
+
+    public void setPorte(int porte) {
+        this.porte = porte;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public int getSituacaoCadastral() {
+        return situacaoCadastral;
+    }
+
+    public void setSituacaoCadastral(int situacaoCadastral) {
+        this.situacaoCadastral = situacaoCadastral;
+    }
+
+    public String getDataDaSituacao() {
+        return dataDaSituacao;
+    }
+
+    public void setDataDaSituacao(String dataDaSituacao) {
+        this.dataDaSituacao = dataDaSituacao;
+    }
+
+    public int getIndicadorMatriz() {
+        return indicadorMatriz;
+    }
+
+    public void setIndicadorMatriz(int indicadorMatriz) {
+        this.indicadorMatriz = indicadorMatriz;
+    }
+
     @Override
     public String toString() {
-        return "Administracao{" + "id=" + id + ", cnpj=" + cnpj + ", dataDeAbertura=" + dataDeAbertura + ", razaoSocial=" + razaoSocial + '}';
-    }    
+        return "Administracao{" + "id=" + id + ", cnpj=" + cnpj + ", dataDeAbertura=" + dataDeAbertura + ", razaoSocial=" + razaoSocial + ", nomeFantasia=" + nomeFantasia + ", porte=" + porte + ", email=" + email + ", telefone=" + telefone + ", situacaoCadastral=" + situacaoCadastral + ", dataDaSituacao=" + dataDaSituacao + ", indicadorMatriz=" + indicadorMatriz + '}';
+    }
     
 }
--- Projeto-v1/src/main/java/com/mycompany/projeto/v1/Main.java
+++ Projeto-v1/src/main/java/com/mycompany/projeto/v1/Main.java
@@ -6,6 +6,7 @@ package com.mycompany.projeto.v1;
 
 import classes.Administracao;
 import classes.Endereco;
+import interfaces.CadAdministracao;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -16,80 +17,9 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
         
-        Scanner scan = new Scanner(System.in);
-        
-        ArrayList<Administracao> testeAdministracao = new ArrayList<>();
-        ArrayList<Endereco> testeEndereco = new ArrayList<>();
-        boolean exec = true;
-        
-        while(exec){
-            
-            System.out.println("");
-            
-            int op = 0;
-            
-            System.out.println("========== Menu ==========");
-            System.out.println("1 - Cadastrar Nova Administração");
-            System.out.println("2 - Ver Administrações Cadastradas");
-            System.out.println("3 - Ver Endereços Cadastrados");
-            System.out.print("Opção: ");
-            op = scan.nextInt();
-            
-            if(op == 1){
-                System.out.println("========== Nova Administração ==========");
-                System.out.print("CNPJ: ");
-                String cnpj = scan.next();
-                System.out.print("Data de Abertura: ");
-                String data = scan.next();
-                System.out.print("Razão Social: ");
-                String razaoSocial = scan.next();
-                System.out.println("");
-                scan.nextLine();
-                System.out.print("CEP: ");
-                String cep = scan.next();
-                System.out.print("Logradouro: ");
-                String logradouro = scan.next();
-                scan.nextLine();
-                System.out.print("Número: ");
-                int numero = scan.nextInt();
-                System.out.print("Complento: ");
-                String complemento = scan.next();
-                scan.nextLine();
-                System.out.print("Bairro: ");
-                String bairro = scan.next();
-                scan.nextLine();
-                System.out.print("Município: ");
-                String municipio = scan.next();
-                System.out.print("UF: ");
-                String uf = scan.next();
-                
-                
-                int idAdm = testeAdministracao.size() + 1;
-                int idEnd = testeEndereco.size() + 1;
-                
-                Administracao administracao = new Administracao(idAdm, cnpj, data, razaoSocial);
-                testeAdministracao.add(administracao);
-                Endereco endereco = new Endereco(idEnd, cep, logradouro, numero, complemento, bairro, municipio, uf);
-                testeEndereco.add(endereco);
-                
-            } else if(op == 2){
-                for(Administracao administracao:testeAdministracao){
-                    System.out.println(administracao.toString());
-                }
-            } else if(op == 3){
-                for(Endereco endereco : testeEndereco){
-                    System.out.println(endereco.toString());
-                }
-            } else if(op == 0){
-                exec = false;
-            }
-            
-            System.out.println("");
-            
-        }
-        
+        CadAdministracao cadAdministracao = new CadAdministracao();
+        cadAdministracao.setVisible(true);        
         
     }
 }
