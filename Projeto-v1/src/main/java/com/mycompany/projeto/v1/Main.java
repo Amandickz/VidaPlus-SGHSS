90e1f4b2400908ce621c1f96f4dd8fe591b4d220
Amandickz <amandickz@gmail.com>
2025-06-11 20:30:30 -0300

    Teste de Geração de horários para consultas

--- Projeto-v1/src/main/java/com/mycompany/projeto/v1/Main.java
+++ Projeto-v1/src/main/java/com/mycompany/projeto/v1/Main.java
@@ -6,6 +6,7 @@ package com.mycompany.projeto.v1;
 
 import gerenciamento.GerenciamentoHospitalar;
 import interfaces.TelaInicial;
+import java.util.Scanner;
 
 /**
  *
@@ -15,10 +16,28 @@ public class Main {
 
     public static void main(String[] args) {
         
-        GerenciamentoHospitalar admHospital = new GerenciamentoHospitalar();
+        
+        Scanner scan = new Scanner(System.in);
+        
+        System.out.println("Digite o horário inicial:");
+        int inicial = scan.nextInt();
+        System.out.println("Digite o horário final:");
+        int hfinal = scan.nextInt();
+        System.out.println("Digite o intervalo de minutos entre uma consulta e outra:");
+        int intervalo = scan.nextInt();
+        
+        System.out.println();
+        System.out.println("---- Tabela de Horas ----");
+        for(int i = inicial; i <= hfinal; i++){
+            for(int j = 00; j < 60; j = j + intervalo){
+                System.out.println(i + ":" + j);
+            }
+        }
+        
+        /*GerenciamentoHospitalar admHospital = new GerenciamentoHospitalar();
 
         TelaInicial telaInicial = new TelaInicial(admHospital);
-        telaInicial.setVisible(true);
+        telaInicial.setVisible(true);*/
       
     }
 }
